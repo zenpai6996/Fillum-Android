eba60fba462c559a33ee81c6b99015ebeedcd79a
SRB <souhardaroybarman05@gmail.com>
2025-02-15 12:50:11 +0530

    Optimize code

--- screens/movieScreen.js
+++ screens/movieScreen.js
@@ -124,9 +124,9 @@ export default function MovieScreen(){
         </Text>
       </View>
       {/* cast */}
-            <Cast navigation={navigation} cast={cast}/>
+            {cast.length>0 && <Cast navigation={navigation} cast={cast}/>}
       {/* similar movies */}
-      <MovieList title="Similar Movies" hideSeeAll={true} data={similarMovies}/>
+      {similarMovies.length>0 && <MovieList title="Similar Movies" hideSeeAll={true} data={similarMovies}/>}
     </ScrollView>
   )
 }
